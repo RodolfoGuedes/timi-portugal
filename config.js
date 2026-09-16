7502aed5f49f637460de91d7a0688c7a938ec8fe
RodolfoGuedes <rodolfo.guedes.dev@gmail.com>
2026-09-16 23:03:01 +0200

    Configure direct Telegram contact link

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 // Links principais da landing page.
 const TIMI_CONFIG = {
   affiliateUrl: 'https://timihqs.com/#/pages/login/registerView?code=d3of54',
-  telegramUrl: '#'
+  telegramUrl: 'https://t.me/rodolfoguedes'
 };
